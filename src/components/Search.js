a63ae4400988e7ad447a20593e5dd5d21e42c640
Leonardo Vásquez <leonardovavi@gmail.com>
2018-11-01 23:19:55 -0600

    Added Error Message

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,8 @@ import {Form, Button, FormGroup, FormControl } from 'react-bootstrap';
 
 class Search extends Component {
   state = {
-    userName: ''
+    userName: '',
+    error: ''
   }
 
   handleSubmit = event => {
@@ -15,19 +16,23 @@ class Search extends Component {
       .get(`https://api.github.com/users/${this.state.userName}`)
       .then(resp => {
         this.props.onSubmit(resp.data);
-        this.setState({userName: ''});
+        this.setState({userName: '', error: ''});
       })
-      .catch(err => {console.log(err.message)});
+      .catch(err => {
+        this.setState({error: err});
+      });
   }
 
   render(){
+    const { error } = this.state;
     return (
-      <Form inline onSubmit = {this.handleSubmit}>
+      <Form inline ref={form => this.formEl = form} onSubmit = {this.handleSubmit}>
         <FormGroup controlId="formInlineEmail">
           <FormControl type="text" placeholder="Enter username" value={this.state.userName}
             onChange={event => this.setState({userName: event.target.value})} required/>
         </FormGroup>{' '}
         <Button type="submit">Add</Button>
+        { error && (<p style={{color: 'red'}}> User Not Found!</p>)}
       </Form>
     )
   }
